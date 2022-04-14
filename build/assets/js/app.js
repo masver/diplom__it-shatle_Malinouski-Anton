ab8303c0d742d997ed00fcb5c199897414172232
masver <malina160595@gmail.com>
2022-04-14 21:49:47 +0300

    save

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -3,3 +3,6 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 100,
 });
+const menu = document.getElementById('menu'),
+    close = document.getElementById('burger-close'),
+    open = document.getElementById('burger-close');
