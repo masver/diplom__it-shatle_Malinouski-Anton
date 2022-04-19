b3392bfcec8c40e78efe848e5c89355b517805a1
masver <malina160595@gmail.com>
2022-04-19 22:15:58 +0300

    sabe

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,8 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 100,
-});
-const menu = document.getElementById('menu'),
-    close = document.getElementById('burger-close'),
-    open = document.getElementById('burger-close');
+
+const menu = document.getElementById('menu');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+
+window.ontouchstart = function(event){
+    if(event.targert == close){
+        menu.classList.add('header__menu--close');
+    }
+    if(event.target == open){
+        menu.classList.remove('header__menu--close');
+    }
+}
